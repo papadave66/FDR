78395ec37deb33b034ffad09b8894eb27e36febe
WHR <msl0000023508@gmail.com>
2018-10-04 20:40:25 +0800

    Use java.net.NetworkInterface to get interfaces

--- src/org/rivoreo/fdr/client/PPPoE.java
+++ src/org/rivoreo/fdr/client/PPPoE.java
@@ -1,42 +1,26 @@
 package org.rivoreo.fdr.client;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 import java.util.Set;
 import java.util.HashSet;
 
 class PPPoE {
-	private static String[] get_ethernet_interfaces_from_ifconfig() {
-		System.err.println("method: get_ethernet_interfaces_from_ifconfig()");
+	public static String[] get_ethernet_interfaces() {
 		Set<String> if_set = new HashSet<String>();
 		try {
-			Process p = Runtime.getRuntime().exec(new String[] { "ifconfig", "-a" });
-			System.err.println(p);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String line;
-			while((line = reader.readLine()) != null) {
-				System.err.println(line);
-				if(!Character.isLetter(line.charAt(0))) continue;
-				int colon_i = line.indexOf(':');
-				if(colon_i == -1) continue;
-				if_set.add(line.substring(0, colon_i));
+			Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+			while(interfaces.hasMoreElements()) {
+				NetworkInterface i = interfaces.nextElement();
+				System.err.println(i);
+				if_set.add(i.getName());
 			}
-		} catch(IOException e) {
+		} catch(SocketException e) {
 			e.printStackTrace();
 			return new String[0];
-		} 
+		}
 		String[] a = new String[if_set.size()];
 		return if_set.toArray(a);
-	}
-
-	public static String[] get_ethernet_interfaces() {
-		if(Platform.have_linux()) {
-			return new String[] { "eth0" };
-		} else {
-			return get_ethernet_interfaces_from_ifconfig();
-		}
 	}
 }
