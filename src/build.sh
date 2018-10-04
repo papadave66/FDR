a9db857831c104ae2bf0b6b16af6cc8e4da2fea6
WHR <msl0000023508@gmail.com>
2018-10-04 19:25:37 +0800

    build.sh: cd to correct directory before run javac(1)

--- src/build.sh
+++ src/build.sh
@@ -3,5 +3,7 @@
 SOURCES="org/rivoreo/fdr/client/LoginWindow.java
 org/rivoreo/fdr/client/PPPoE.java
 org/rivoreo/fdr/client/Platform.java"
+set -e
+cd "`dirname \"$0\"`"
 [ -d ../classes ] || mkdir ../classes
 exec javac -d ../classes/ $SOURCES
