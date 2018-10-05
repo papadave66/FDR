40856d1da05274bf65ea79d60b2cebb2c92d1aff
WHR <msl0000023508@gmail.com>
2018-10-05 11:43:45 +0800

    Implement PPP via pppd(8)

--- src/org/rivoreo/fdr/client/PPP.java
+++ src/org/rivoreo/fdr/client/PPP.java
@@ -1,15 +1,15 @@
 package org.rivoreo.fdr.client;
 
-import java.util.AbstractMap;
+//import java.util.AbstractMap;
 
 interface PPP {
-	//PPP();
-	//PPP(boolean use_pty, String device_or_command);
 	void set_device_path(String device);
 	void set_pty_command(String command);
 	void set_mtu(int mtu);
-	void set_chap(AbstractMap.SimpleEntry<String, String> secret);
-	void set_pap(AbstractMap.SimpleEntry<String, String> secret);
+	void set_user(String user);
+	void set_password(String password);
+	//void set_chap(AbstractMap.SimpleEntry<String, String> secret);
+	//void set_pap(AbstractMap.SimpleEntry<String, String> secret);
 	void set_default_route(boolean v);
 	void set_accomp(boolean v);
 	void set_default_asyncmap(boolean v);
--- src/org/rivoreo/fdr/client/PualsPPPPackage.java
+++ src/org/rivoreo/fdr/client/PualsPPPPackage.java
@@ -1,6 +1,9 @@
 package org.rivoreo.fdr.client;
 
+import java.io.IOException;
 import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
 
 class PualsPPPPackage implements PPP {
 	public PualsPPPPackage() {
@@ -11,18 +14,22 @@ class PualsPPPPackage implements PPP {
 		this.device_or_command = device_or_command;
 	}
 
+	private Process pppd_process;
+
 	private boolean use_pty;
 
 	private String device_or_command = null;
 	private int mtu = -1;
-	private AbstractMap.SimpleEntry<String, String> chap_secret = null;
-	private AbstractMap.SimpleEntry<String, String> pap_secret = null;
+	private String user = null;
+	private String password = null;
+	//private AbstractMap.SimpleEntry<String, String> chap_secret = null;
+	//private AbstractMap.SimpleEntry<String, String> pap_secret = null;
 	private boolean refuse_chap = false;
 	private boolean refuse_pap = false;
-	private boolean default_route;
-	private boolean accomp;
-	private boolean default_asyncmap;
-	private boolean persist;
+	private boolean default_route = false;
+	private boolean accomp = true;
+	private boolean default_asyncmap = false;
+	private int persist = -1;
 	private int speed = -1;
 
 	public void set_device_path(String device) {
@@ -39,6 +46,15 @@ class PualsPPPPackage implements PPP {
 		this.mtu = mtu;
 	}
 
+	public void set_user(String user) {
+		this.user = user;
+	}
+
+	public void set_password(String password) {
+		this.password = password;
+	}
+
+/*
 	public void set_chap(AbstractMap.SimpleEntry<String, String> secret) {
 		if(secret == null) refuse_chap = true;
 		else {
@@ -54,7 +70,7 @@ class PualsPPPPackage implements PPP {
 			pap_secret = secret;
 		}
 	}
-
+*/
 	public void set_default_route(boolean v) {
 		default_route = v;
 	}
@@ -68,7 +84,7 @@ class PualsPPPPackage implements PPP {
 	}
 
 	public void set_persist(boolean v) {
-		persist = v;
+		persist = v ? 1 : 0;
 	}
 
 	public void set_speed(int speed) {
@@ -77,12 +93,54 @@ class PualsPPPPackage implements PPP {
 
 	public void dial() throws MissingDeviceException {
 		if(device_or_command == null) throw new MissingDeviceException();
+		List<String> args = new ArrayList<String>();
+		args.add("pppd");
+		if(mtu != -1) {
+			args.add("mtu");
+			args.add(String.valueOf(mtu));
+		}
+		if(refuse_chap) args.add("refuse-chap");
+		if(refuse_pap) args.add("refuse-pap");
+		args.add(default_route ? "defaultroute" : "nodefaultroute");
+		if(!accomp) args.add("noaccomp");
+		if(default_asyncmap) args.add("default-asyncmap");
+		if(persist != -1) args.add(persist >= 1 ? "persist" : "nopersist");
+		if(use_pty) {
+			args.add("pty");
+			args.add(device_or_command);
+		} else {
+			args.add(device_or_command);
+			if(speed != -1) args.add(String.valueOf(speed));
+		}
+		if(user != null) {
+			args.add("user");
+			args.add(user);
+		}
+		if(password != null) {
+			args.add("password");
+			args.add(password);
+		}
+		args.add("nodetach");
+		String[] a = new String[args.size()];
+		try {
+			pppd_process = Runtime.getRuntime().exec(args.toArray(a));
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void hangup() {
+		if(pppd_process == null) return;
+		pppd_process.destroy();
 	}
 
 	public boolean is_connected() {
+		if(pppd_process == null) return false;
+		try {
+			pppd_process.exitValue();
+		} catch(IllegalThreadStateException e) {
+			return true;
+		}
 		return false;
 	}
 }
