22711f7f5ce4c1574a94ba7b88f2baceb544aeac
papadave66 <papadave@foxmail.com>
2018-10-05 17:41:02 +0800

    add a statusWindow with swing

---
+++ src/org/rivoreo/fdr/client/StatusWindow.java
@@ -0,0 +1,27 @@
+package org.rivoreo.fdr.client;
+
+import javax.swing.*;
+
+/**
+ * Created by papadave on 2018/10/5.
+ */
+public class StatusWindow {
+    private JPanel panel1;
+    private JLabel JLabelUsernameText;
+    private JLabel JLableUsername;
+    private JLabel JLabelNetworkplanText;
+    private JLabel JLabelNetworkplan;
+    private JLabel JLabelTimerText;
+    private JLabel JLabelTimer;
+    private JButton BtnDisconnect;
+    private JButton BtnWebsite;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("status");
+        frame.setContentPane(new StatusWindow().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(300,300);
+    }
+}
