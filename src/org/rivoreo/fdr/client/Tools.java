7069c1e418a6a8bc3a0099b4641924e36dd0c07f
papadave66 <papadave@foxmail.com>
2018-10-08 08:34:51 +0800

    add a tool class . To make the whole code more clear.

---
+++ src/org/rivoreo/fdr/client/Tools.java
@@ -0,0 +1,8 @@
+package org.rivoreo.fdr.client;
+
+/**
+ * Created by papadave on 2018/10/8.
+ */
+public class Tools {
+    public static String [] NETWORK_PLAN = {"教育网2M", "联通20M", "联通合约30M", "联通合约50M","电信10M","电信合约10M"};
+}
